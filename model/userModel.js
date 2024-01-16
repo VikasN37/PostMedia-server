88d63fc5b6144bc15180f20159801b795df3289d
Vikas <vikas630602@gmail.com>
2024-01-16 14:26:52 +0530

    final fixes

--- model/userModel.js
+++ model/userModel.js
@@ -44,8 +44,6 @@ const userSchema = new mongoose.Schema(
 
     profilePhoto: {
       type: String,
-      default:
-        'https://i.pinimg.com/originals/0d/64/98/0d64989794b1a4c9d89bff571d3d5842.jpg',
     },
 
     passwordChangedAt: {
